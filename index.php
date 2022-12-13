614944a5635dd3b1ea3263828ebed304bbad29aa
RoseleiBrancher <rosebr3107@gmail.com>
2022-12-13 10:32:42 -0300

    Projeto 4 - Hectra

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hectra Contabilidade Estratégica</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+    <section class="topo">
+        <div class="center">
+            <header>
+                <div class="logo"><img src="imagens/logo.png" /></div>
+            </header>
+            <ul class="menu-desktop">
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+            <div class="clear"></div>
+            <br />
+            <br />
+            <div class="w50">
+                <h2>Há 30 anos proporcionando segurança e credibilidade a todos os seus clientes</h2>
+                <p>Equipe profissional e especializada para atender a sua empresa com excelência!</p>
+                <a target="_black" href="https://www.facebook.com/profile.php?id=100070930892134">Ver demonstração</a>
+            </div>
+            <div class="w50">
+                <img src="imagens/equipe.png" />
+            </div>
+            <div class="clear"></div>
+        </div>      
+    </section>
+</body>
+</html>
